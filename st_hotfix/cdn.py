b3c7be3e2a44cefb56640a33e0384d8b5fb503aa
weihong.xu <xuweihong.cn@gmail.com>
2024-08-01 01:10:15 +0800

    wip: toolkit for cdn

---
+++ st_hotfix/cdn.py
@@ -0,0 +1,23 @@
+import importlib
+
+
+def dump(pkg_name: str, out_dir= './out'):
+    """
+    Find static assets in the package and dump them to the out_dir.
+    """
+    
+
+def apply(pkg_name: str, cdn_url: str):
+    ...
+
+
+def restore(pkg_name: str):
+    ...
+
+
+class StreamlitPkg:
+    ...
+
+    
+
+    
